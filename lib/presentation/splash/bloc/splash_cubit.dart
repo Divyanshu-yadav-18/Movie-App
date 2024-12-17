ac8ff1c3b28147543c8d5215324ce311ba0622bf
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2024-12-17 17:16:18 +0530

    feat: setup bloc

---
+++ lib/presentation/splash/bloc/splash_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/presentation/splash/bloc/splash_state.dart';
+
+class SplashCubit extends Cubit<SplashState> {
+  SplashCubit() : super(DisplaySplash());
+
+  void appStarted() async {
+    await Future.delayed(
+      const Duration(
+        milliseconds: 200,
+      ),
+    );
+    emit(
+      Authenticated(),
+    );
+  }
+}
--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_app/core/configs/assets/app_image.dart';
+import 'package:movie_app/presentation/splash/bloc/splash_cubit.dart';
+import 'package:movie_app/presentation/splash/bloc/splash_state.dart';
 
 class splashPage extends StatelessWidget {
   const splashPage({super.key});
@@ -7,28 +10,34 @@ class splashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(AppImage.splashBackground),
+      body: BlocListener<SplashCubit, SplashState>(
+        listener: (context, state) {
+          if (state is Unauthenticated) {}
+          if (state is Authenticated) {}
+        },
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(AppImage.splashBackground),
+                ),
               ),
             ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.center,
-                end: Alignment.bottomCenter,
-                colors: [
-                  const Color(0xff1A1B20).withOpacity(0),
-                  const Color(0xff1A1B20),
-                ],
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.center,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    const Color(0xff1A1B20).withOpacity(0),
+                    const Color(0xff1A1B20),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
