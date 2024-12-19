c75041e630ae0be53b69b2ba74adb3e027fe2496
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2024-12-19 22:02:54 +0530

    setup services

---
+++ lib/data/sources/auth/auth_api_service.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:movie_app/core/network/dio_client.dart';
+import 'package:movie_app/service_locator.dart';
+
+abstract class AuthApiService {
+  Future<Either> signup();
+}
+
+class AuthApiServiceImpl extends AuthApiService {
+  @override
+  Future<Either> signup() async {
+    try {
+      var response = await sl<DioClient>().post(url);
+    } on DioException catch (e) {
+      return Left(e.response!.data['message']);
+    }
+  }
+}
