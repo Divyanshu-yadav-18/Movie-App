6f36f5fe653ef5039e57f18ff0f80a82c3772cd7
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-01-10 22:56:21 +0530

    trending movies ui

---
+++ lib/presentation/home/widgeta/trending.dart
@@ -0,0 +1,41 @@
+import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/core/configs/assets/app_image.dart';
+import 'package:movie_app/presentation/home/block/trending_cubit.dart';
+import 'package:movie_app/presentation/home/block/trending_state.dart';
+
+class TrendingMovies extends StatelessWidget {
+  const TrendingMovies({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TrendingCubit()..getTrendingMovies(),
+      child: BlocBuilder<TrendingCubit, TrendingState>(
+        builder: (context, state) {
+          if (state is TrendingMoviesLoading) {
+            return CircularProgressIndicator();
+          }
+          if (state is TrendingMoviesLoaded) {
+            return FanCarouselImageSlider.sliderType1(
+              imagesLink: state.movies
+                  .map((item) =>
+                      AppImage.movieImageBasePath + item.posterPath.toString())
+                  .toList(),
+              isAssets: false,
+              autoPlay: false,
+              sliderHeight: 400,
+              showIndicator: true,
+            );
+          }
+          if (state is FailureLoadTrendingMovies) {
+            return Text(state.errorMessage);
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+}
