1c5b0467cd1a5aa651cb70b3f89d31abd344e51f
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-01-16 19:04:47 +0530

    minor changes in cubit

--- lib/presentation/watch/bloc/trailer_cubit.dart
+++ lib/presentation/watch/bloc/trailer_cubit.dart
@@ -1,8 +1,11 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/core/constant/api_url.dart';
 import 'package:movie_app/core/entity/trailer.dart';
 import 'package:movie_app/domain/movie/usecases/get_movie_trailer.dart';
 import 'package:movie_app/presentation/watch/bloc/trailer_state.dart';
 import 'package:movie_app/service_locator.dart';
+import 'package:video_player/video_player.dart';
 
 class TrailerCubit extends Cubit<TrailerState> {
   TrailerCubit() : super(TrailerLoading());
@@ -11,8 +14,16 @@ class TrailerCubit extends Cubit<TrailerState> {
     var returedData = await sl<GetMovieTrailerUseCase>().call(params: movieId);
     returedData.fold((error) {
       emit(LoadFailureTrailer(errorMessage: error));
-    }, (data) {
+    }, (data) async {
       TrailersEntity trailersEntity = data;
+      VideoPlayerController videoPlayerController =
+          VideoPlayerController.networkUrl(
+              Uri.tryParse(ApiUrl.trailerBase + trailersEntity.key!)!);
+      await videoPlayerController.initialize();
+      final chewieController = ChewieController(
+          videoPlayerController: videoPlayerController,
+          autoPlay: false,
+          looping: false);
       emit(TrailerLoaded(chewieController: chewieController));
     });
   }
